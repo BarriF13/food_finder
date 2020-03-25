f11ed820d0cd1b454fbc0c8ac5f96dd734bb7c24
Barri <info@bdesignstudio.co.uk>
2020-03-25 19:04:03 +0000

    setting up the user choice menu

--- lib/guide.rb
+++ lib/guide.rb
@@ -14,18 +14,41 @@ class Guide
         #exit if create fails
         puts "Exiting ..\n\n"
         exit!
-      end
-        
+      end  
   end
 
   def launch
     introduction
-    # action loop do
+    result = nil
+    until result == :quit 
+    
     # what do you want to do ?(list, find, add , quit)
+    print "> "
+    user_response = gets.chomp
     #do that action
+    result = do_action(user_response)
     # repeat until user quits
+    end
     conclusion
-  
+  end
+
+  def get_action
+    
+
+  def do_action(action)
+    case action
+    when 'list'
+      puts 'Listing..'
+    when 'find'
+      puts 'Finding..'
+    when 'add '
+      puts 'Adding ..'
+    when 'quit'
+      return :quit
+    else
+      puts "\nI don not understand that command.\n"
+    end
+
   end
 
   def introduction
