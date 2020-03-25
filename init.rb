0a2644942940c169f10cbea29deb6e534bf52fd4
Barri <info@bdesignstudio.co.uk>
2020-03-25 16:51:27 +0000

    Set up the app launcher

---
+++ init.rb
@@ -0,0 +1,15 @@
+#### Food Finder ####
+#
+#launch this Ruby file from command line
+#to get started.
+#
+APP_ROOT = File.dirname(__FILE__)
+
+#require "#{APP_ROOT}/lib/guide"
+#require FILE.join(APP_ROOT, 'lib', 'guide')
+
+$:.unshift(File.join(APP_ROOT, 'lib'))
+require 'guide'
+
+guide = Guide.new('restaurant.txt')
+guide.launch
